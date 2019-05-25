98279ee9e392cbdc8cae94d162bd549f2ca7e183
Jeff Goldschrafe <jeff@holyhandgrenade.org>
2019-05-24 23:19:30 -0400

    Sherlock and Anagrams
    https://www.hackerrank.com/challenges/sherlock-and-anagrams/problem

---
+++ sherlock-and-anagrams/src/main.rs
@@ -0,0 +1,50 @@
+use std::collections::HashMap;
+use std::io;
+use std::iter::FromIterator;
+
+// precomputed nCr, r = 2
+const coefficients: [u32;100] = [
+       0,    1,    3,    6,   10,   15,   21,   28,   36,   45,
+      55,   66,   78,   91,  105,  120,  136,  153,  171,  190,
+     210,  231,  253,  276,  300,  325,  351,  378,  406,  435, 
+     465,  496,  528,  561,  595,  630,  666,  703,  741,  780,
+     820,  861,  903,  946,  990, 1035, 1081, 1128, 1176, 1225,
+    1275, 1326, 1378, 1431, 1485, 1540, 1596, 1653, 1711, 1770,
+    1830, 1891, 1953, 2016, 2080, 2145, 2211, 2278, 2346, 2415,
+    2485, 2556, 2628, 2701, 2775, 2850, 2926, 3003, 3081, 3160,
+    3240, 3321, 3403, 3486, 3570, 3655, 3741, 3828, 3916, 4005,
+    4095, 4186, 4278, 4371, 4465, 4560, 4656, 4753, 4851, 4950,
+];
+
+fn anagram_substrings(s: &str) -> u32 {
+    let mut repetitions: HashMap<String, u32> = HashMap::new();
+    for substr_len in 1..=s.len() {
+        for start_pos in 0..=(s.len() - substr_len) {
+            let end_pos = start_pos + substr_len;
+            let substr = String::from(&s[start_pos..end_pos]);
+
+            let mut chars: Vec<char> = substr.chars().collect();
+            chars.sort_by(|a, b| a.cmp(b));
+            let sorted = String::from_iter(chars);
+
+            *(repetitions.entry(sorted).or_insert(0)) += 1;
+        }
+    }
+
+    repetitions.values()
+        .into_iter()
+        .map(|n| coefficients[(*n - 1) as usize])
+        .fold(0u32, |sum, val| sum + val)
+}
+
+fn main() {
+    let mut queries_buf = String::new();
+    io::stdin().read_line(&mut queries_buf).unwrap();
+    let queries: u8 = queries_buf.trim_end().parse().unwrap();
+
+    for len in 0..queries {
+        let mut s = String::new();
+        io::stdin().read_line(&mut s).unwrap();
+        println!("{}", anagram_substrings(&(s.trim_end()[..])));
+    }
+}
